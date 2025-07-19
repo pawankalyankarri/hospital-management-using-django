84886e05136d0e1a54804a108e281fd1c2ef213d
pawankalyan <pawankalyankarri22@gmail.com>
2025-07-19 23:40:09 +0530

    added few styles

--- HospitalManagement/urls.py
+++ HospitalManagement/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/',include('loginpage.urls')),
-    path('home/',include('homepage.urls'))
+    path('',include('homepage.urls'))
 ]
 
 urlpatterns = urlpatterns+static(settings.MEDIA_URL,document_root = settings.MEDIA_ROOT)
